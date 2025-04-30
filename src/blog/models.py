c1a0df9ec5642a8fe17fbe76f0d8f25c559bd189
Vania <sonalysifuentes@gmail.com>
2025-04-30 14:06:49 -0500

    defined models

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,131 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    """Category model for organizing blog posts"""
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    
+    class Meta:
+        verbose_name_plural = "categories"
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('blog:category_detail', args=[self.slug])
+
+
+class Tag(models.Model):
+    """Tag model for categorizing blog posts"""
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+    
+    class Meta:
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('blog:tag_detail', args=[self.slug])
+
+
+class PostManager(models.Manager):
+    """Custom manager for Post model"""
+    
+    def published(self):
+        """Return only published posts"""
+        return self.filter(status='published')
+    
+    def by_category(self, category_slug):
+        """Return posts for a specific category"""
+        return self.published().filter(category__slug=category_slug)
+    
+    def by_tag(self, tag_slug):
+        """Return posts with a specific tag"""
+        return self.published().filter(tags__slug=tag_slug).distinct()
+    
+    def recent_posts(self, count=5):
+        """Return most recent posts"""
+        return self.published().order_by('-published_date')[:count]
+
+
+class Post(models.Model):
+    """Blog post model"""
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    
+    # Relationships
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+    
+    # Custom manager
+    objects = models.Manager()  # Default manager
+    published_objects = PostManager()  # Custom manager
+    
+    class Meta:
+        ordering = ['-published_date']
+        indexes = [
+            models.Index(fields=['-published_date']),
+        ]
+    
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.slug])
+    
+    @property
+    def comment_count(self):
+        """Return the number of comments for this post"""
+        return self.comments.filter(approved=True).count()
+
+
+class Comment(models.Model):
+    """Comment model for blog posts"""
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['created_date']
+        indexes = [
+            models.Index(fields=['created_date']),
+        ]
+    
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
