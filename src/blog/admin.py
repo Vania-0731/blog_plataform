192fe76a66be8c391424caf34cc684e02130b8c2
Eduardo <eduardobullonvera@gmail.com>
2025-04-30 14:15:38 -0500

    Implementing Admin Interface

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Category, Tag, Post, Comment
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'status', 'published_date')
+    list_filter = ('status', 'category', 'published_date', 'author')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'published_date'
+    filter_horizontal = ('tags',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created_date', 'approved')
+    list_filter = ('approved', 'created_date')
+    search_fields = ('name', 'email', 'content')
+    actions = ['approve_comments']
+    
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+    approve_comments.short_description = "Approve selected comments"
